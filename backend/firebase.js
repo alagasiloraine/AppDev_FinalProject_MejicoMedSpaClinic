5a0f3a702ef9dcfd1a4f34bac14579802c72c0ec
rosalyncapio <rosalyncapio88@gmail.com>
2024-10-24 13:14:18 +0800

    initial commit

---
+++ backend/firebase.js
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./config/serviceAccountKey.json'); // Replace with the actual path to your Firebase credentials file
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mejicoclinic-e03b2-default-rtdb.firebaseio.com" // Add this line
+});
+
+// Export the initialized Firebase admin instance for use in other files
+module.exports = admin;
